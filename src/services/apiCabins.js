d3ce0dc9cb70b14a35dc038444fb577095b5f354
olah <olahisrael07@gmail.com>
2024-10-01 11:04:01 +0100

    refactored form row

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -13,6 +13,21 @@ export async function getCabins() {
         return data;
 }
 
+export async function CreateCabin(newCabin) {
+
+const { data, error } = await supabase
+.from('cabins')
+.insert([newCabin]);
+
+if(error) {
+    console.error(error);
+    throw new Error("Cabin could not be created");
+}
+
+return data;
+
+}
+
 export async function deleteCabin(id) {
     const { data, error } = await supabase
     .from('cabins')
