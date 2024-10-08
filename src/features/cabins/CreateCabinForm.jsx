d3edd23c22a8466fa04d28cde8f1eb60a1f80d87
olah <olahisrael07@gmail.com>
2024-10-08 15:07:06 +0100

    Added modal window to cabin table component

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -11,7 +11,7 @@ import FormRow from "../../ui/FormRow";
 
 
 
-function CreateCabinForm({cabinToEdit = {}}) {
+function CreateCabinForm({cabinToEdit = {}, onCloseModal}) {
     const { isCreating, createCabin } = useCreateCabin();
     const { isEditing, editCabin} = useEditCabin();
     const isWorking = isCreating || isEditing;
@@ -31,12 +31,14 @@ function CreateCabinForm({cabinToEdit = {}}) {
       if(isEditSession) editCabin({newCabinData: {...data, image}, id: editId}, {
         onSuccess: (data) => {
           reset();
+          onCloseModal?.();
         }
       });
 
       else createCabin({...data, image: data.image[0] }, {
         onSuccess: (data) => {
           reset();
+          onCloseModal?.();
         }
       });
     }
@@ -46,7 +48,7 @@ function CreateCabinForm({cabinToEdit = {}}) {
     }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+    <Form onSubmit={handleSubmit(onSubmit, onError)} type={onCloseModal ? "modal" : "regular"}>
       <FormRow label="Cabin name" error={errors?.name?.message}>
         <Input type="text" id="name" disabled={isWorking} {...register("name", { required: "This field is required",})} />
       </FormRow>
@@ -73,7 +75,7 @@ function CreateCabinForm({cabinToEdit = {}}) {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation="secondary" type="reset" onClick={() => onCloseModal?.()}>
           Cancel
         </Button>
         <Button disabled={isWorking}>{isEditSession ? "Edit cabin" : "Create new cabin"}</Button>
