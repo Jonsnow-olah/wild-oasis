ea5497ac1442b8b4c5e5fa63831e010d7200d11d
olah <olahisrael07@gmail.com>
2024-10-09 15:44:14 +0100

    Added confirmation function to delete a table

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,28 +1,29 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
-import CabinTable from "./CabinTable";
+
 
 function AddCabin() {
     return (
-        <Modal>
-            <Modal.Open opens="cabin-form">
-                <Button>Add new cabin</Button>
-            </Modal.Open>
+        <div>
+            <Modal>
+                <Modal.Open opens="cabin-form">
+                    <Button>Add new cabin</Button>
+                </Modal.Open>
 
-            <Modal.Window name="cabin-form">
-                <CreateCabinForm />
-            </Modal.Window>
+                <Modal.Window name="cabin-form">
+                    <CreateCabinForm />
+                </Modal.Window>
 
-            {/* <Modal.Open opens="table">
-                <Button>Show table</Button>
-            </Modal.Open>
+                {/* <Modal.Open opens="table">
+                    <Button>Show table</Button>
+                </Modal.Open>
 
-            <Modal.Window name="table">
-                <CabinTable />
-            </Modal.Window> */}
-        </Modal>
+                <Modal.Window name="table">
+                    <CabinTable />
+                </Modal.Window> */}
+            </Modal>
+        </div>
     )
 }
 
